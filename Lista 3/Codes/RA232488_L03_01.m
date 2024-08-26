9b09f53c027798ff3d310a4356cdaae7fb861bec
bruno <brunombonetti@hotmail.com>
2024-08-26 16:25:09 -0300

    exercise 1

--- Lista 3/Codes/RA232488_L03_01.m
+++ Lista 3/Codes/RA232488_L03_01.m
@@ -1,17 +1,19 @@
-function d = RA232488_L03_01()
-    
+function [d] = RA232488_L03_01()
+    clc; clear all; close all;
+
     %parametros utilizados
-    k1 = 50e3;
-    k2 = 50;
-    m = 112;
-    g = 9.81
-    h = 0.51;
+    k1 = 40e3;
+    k2 = 45;
+    m = 95;
+    g = 9.81;
+    h = 0.44;
+
+    %equacao
+    f = @(d)  (2/5 * k2 * d^(5/2)) + (1/2 * k1 * d^2) - (m * g * d) - (m * g * h);
     
+    %usando fzero
+    raiz = fzero(f, 0.5);
 
-
+    d = raiz;
     
-
-
-
-
 end
