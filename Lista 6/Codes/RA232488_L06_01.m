53a1b8c0d7eb3e934e688442c7534c7e22e96204
bruno <brunombonetti@hotmail.com>
2024-09-16 16:15:31 -0300

    ex01 finished

---
+++ Lista 6/Codes/RA232488_L06_01.m
@@ -0,0 +1,16 @@
+function [ri] = RA232488_L06_01()
+   
+    clc; clear all; close all;
+    
+    %dados do enunciado
+    q = 85;
+    r_w = 0.00635;
+    k = 0.181;
+    h = 14;
+    T_air = 298;
+    
+    T = @(ri) T_air + q/(2*pi) * (1/k * log((r_w + ri)/r_w) + 1/h * 1/(r_w + ri));
+
+    ri = fminbnd(T, 0, 0.1);
+   
+end
