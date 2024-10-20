5698f73bf1fe1b1e548183f95d53dd48841a0aa4
bruno <brunombonetti@hotmail.com>
2024-10-20 15:32:33 -0300

    lista 10 finally finished

--- Lista 10/Codes/RA232488_L10_03.m
+++ Lista 10/Codes/RA232488_L10_03.m
@@ -19,15 +19,9 @@ function [v_200, a_200, v_206, a_206, v_210, a_210] = RA232488_L10_03()
     derivada_theta_200 = derivada_theta(1); 
     derivada_segunda_r_200 = derivada_segunda_r(1); 
     derivada_segunda_theta_200 = derivada_segunda_theta(1);
-    
-    v_200_er = derivada_r_200;
-    v_200_etheta = r_200 * derivada_theta_200;
-    
-    a_200_er = derivada_segunda_r_200 - r_200 * derivada_theta_200^2;
-    a_200_etheta = r_200 * derivada_segunda_theta_200 + 2 * derivada_r_200 * derivada_theta_200;
 
-    v_200 = [v_200_er; v_200_etheta];
-    a_200 = [a_200_er; a_200_etheta];
+    v_200 = [derivada_r_200; r_200 * derivada_theta_200];
+    a_200 = [(derivada_segunda_r_200 - (r_200 * derivada_theta_200^2)); (r_200 * derivada_segunda_theta_200 + 2 * derivada_r_200 * derivada_theta_200)];
     
     %para t = 206s
     r_206 = r(4);
@@ -36,14 +30,8 @@ function [v_200, a_200, v_206, a_206, v_210, a_210] = RA232488_L10_03()
     derivada_segunda_r_206 = derivada_segunda_r(2); 
     derivada_segunda_theta_206 = derivada_segunda_theta(2);
     
-    v_206_er = derivada_r_206;
-    v_206_etheta = r_206 * derivada_theta_206;
-    
-    a_206_er = derivada_segunda_r_206 - r_206 * derivada_theta_206^2;
-    a_206_etheta = r_206 * derivada_segunda_theta_206 + 2 * derivada_r_206 * derivada_theta_206;
-
-    v_206 = [v_206_er; v_206_etheta];
-    a_206 = [a_206_er; a_206_etheta];
+    v_206 = [derivada_r_206; r_206 * derivada_theta_206];
+    a_206 = [(derivada_segunda_r_206 - (r_206 * derivada_theta_206^2)); (r_206 * derivada_segunda_theta_206 + 2 * derivada_r_206 * derivada_theta_206)];
 
     %para t = 210s
     r_210 = r(6);
@@ -52,13 +40,8 @@ function [v_200, a_200, v_206, a_206, v_210, a_210] = RA232488_L10_03()
     derivada_segunda_r_210 = derivada_segunda_r(4); 
     derivada_segunda_theta_210 = derivada_segunda_theta(4);
     
-    v_210_er = derivada_r_210;
-    v_210_etheta = r_210 * derivada_theta_210;
-    
-    a_210_er = derivada_segunda_r_210 - r_210 * derivada_theta_210^2;
-    a_210_etheta = r_210 * derivada_segunda_theta_210 + 2 * derivada_r_210 * derivada_theta_210;
+    v_210 = [derivada_r_210; r_210 * derivada_theta_210];
+    a_210 = [(derivada_segunda_r_210 - (r_210 * derivada_theta_210^2)); (r_210 * derivada_segunda_theta_210 + 2 * derivada_r_210 * derivada_theta_210)];
 
-    v_210 = [v_210_er; v_210_etheta];
-    a_210 = [a_210_er; a_210_etheta];
 
 end
